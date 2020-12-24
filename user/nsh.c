e0707cfde74663f513ed498f318a985dc1ff954d
caskr <1037602654@qq.com>
2020-12-25 00:47:08 +0800

    lab2 review

--- user/nsh.c
+++ user/nsh.c
@@ -26,7 +26,7 @@ getcmd(char *buf, int nbuf)
   return 0;
 }
 
-int parsecmd(char buf[], char *argv[], int argc){
+int parsecmd(char buf[], char *argv[]){
     
     for(int i = 0; i < MAXARGS; i++){//赋予指针指向
         argv[i] = & args[i][0];
@@ -46,38 +46,39 @@ int parsecmd(char buf[], char *argv[], int argc){
             j++;
         }
 
-        buf[j] = '\0';
+        buf[j] = '\0';  //将空格或换行符写为\0作字符分割
         j++;
         if(strcmp(argv[i],"|")==0){
-            pipe_l[pipe_num++] = i;
+            pipe_l[pipe_num++] = i;//记录管道标志位置
         }
-        i++;
+        i++;//参数个数+1
     }
-    argv[i] = 0;
-    return i;
+    argv[i] = 0;//最后一个为0
+    return i;//返回参数个数
 }
 
 
 void runcmd(char *argv[], int argc){
-    if(pipe_num != pipe_i){
+    if(pipe_num != pipe_i){//先执行管道指令
         runpipe(argv,argc);
+        return;
     }
     for(int i=1;i<argc;i++){
-        if(!strcmp(argv[i],">")){
-            close(1);
+        if(!strcmp(argv[i],">")){//输出重定向
+            close(1);//关闭标准输出
             if(open(argv[i+1], O_CREATE|O_WRONLY) < 0){
                 fprintf(2, "open %s failed\n", argv[i+1]);
                 exit(-1);
             }
-            argv[i]=0;
+            argv[i]=0;//重定向标志设0
         }
-        if(!strcmp(argv[i],"<")){
-            close(0);
+        if(!strcmp(argv[i],"<")){//输入重定向
+            close(0);//关闭标准输入
             if(open(argv[i+1], O_RDONLY) < 0){
             fprintf(2, "open %s failed\n", argv[i+1]);
             exit(-1);
             }
-            argv[i]=0;
+            argv[i]=0;//重定向标志设0
         }
     }
     exec(argv[0],argv);
@@ -85,24 +86,24 @@ void runcmd(char *argv[], int argc){
 
 
 void runpipe(char *argv[],int argc){
-    argv[pipe_l[pipe_i]] = 0;
+    argv[pipe_l[pipe_i]] = 0;//将管道标志设0
     int i = pipe_l[pipe_i++];
     int p[2];
     if (pipe(p) < 0){
         fprintf(2,"pipe create error!\n");
     }
     if(fork() == 0){
-        close(1);
-        dup(p[1]);
+        close(1);//关闭标准输出
+        dup(p[1]);//将管道输入端设为标准输出
         close(p[0]);
         close(p[1]);
-        runcmd(argv,i);
+        runcmd(argv,i);//管道左侧
     }
-    close(0);
-    dup(p[0]);
+    close(0);//关闭标准输入
+    dup(p[0]);//将管道输出端设为标准输入
     close(p[0]);
     close(p[1]);
-    runcmd(argv+i+1,argc-i-1);
+    runcmd(argv+i+1,argc-i-1);//管道右侧
     wait(0);
 }
 
@@ -113,7 +114,7 @@ int main(){
         if(fork()==0){
             char *argv[MAXARGS];
             int argc = -1;
-            argc = parsecmd(buf,argv,argc);
+            argc = parsecmd(buf,argv);
             runcmd(argv,argc);
         }
         wait(0);
